1352a08b769544ca122160251af3b198b9319191
RumenIliev <rumen.iliev50@gmail.com>
2023-05-09 19:23:20 +0300

    Solution.

---
+++ project/Test.py
@@ -0,0 +1,6 @@
+from project.Easter_Bunny import EasterBunny
+
+easter = EasterBunny([])
+print("* Welcome to the automatic magic egg factory! * \n")
+while True:
+    easter.choose_random_class_type()
